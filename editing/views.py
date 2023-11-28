ce87383241b2ec65555d25e2306a6d0d40d13d24
maomail <umenyakrutayakoshka@mail.ru>
2023-11-28 10:11:47 +0300

    Инициализация

--- editing/views.py
+++ editing/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class IndexView(View):
+
+    songs = [
+        {'name': 'Браво - Нет предела', 'text': 'Hello world!', 'section': 'funny'},
+        {'name': 'pyrokinesis - Богиня мечей', 'text': 'Hello world!', 'section': 'sad'},
+        {'name': 'Sirius Eye', 'text': 'Hello world!', 'section': 'beautiful'},
+    ]
+
+    def get(self, request, *args, **kwargs):
+        return render(request, '/main/index.html')
+
+
---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # BEGIN (write your solution here)
+    path('', views.index),
+    # END
+]
